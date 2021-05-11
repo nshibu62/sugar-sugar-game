b82f4fe568403d84a29f6570c22558bc708a3b0e
Glitch (wiry-plaid-valley) <none>
2021-05-11 17:45:07 +0000

    🎇☺️ Checkpoint ./sketch.js:31745905/46

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 /* global createCanvas, loadImage, background, image */
 
 let dvdImage;
+var x;
 
 function setup(){
   createCanvas(800, 600);
+  x = 50;
   // We only want to load the logo once.
   dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
 }
@@ -11,7 +13,5 @@ function setup(){
 function draw(){
   background(220);
   // Draw the logo at the new position.
-  image(dvdImage, 50, 50, 200, 150);
+  image(dvdImage, x, 50, 200, 150);
 }
-
-// HELLO :)
