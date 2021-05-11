e7cf5bf63b428574ce17d7ac6a635caadd4c7e6f
Glitch (wiry-plaid-valley) <none>
2021-05-11 18:19:00 +0000

    🌊🏫 Checkpoint ./sketch.js:31745905/194

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
-/* global createCanvas, loadImage, background, image */
+/* global createCanvas, loadImage, background, image, width, height */
 
 let dvdImage;
 var x, xVelocity;
 var y, yVelocity;
 var masterVelocity;
+var logoWidth, logoHeight;
 
 function setup(){
-  createCanvas(800, 600);
+  createCanvas(300, 600);
   x = 50;
   y = 50;
   masterVelocity = 1;
@@ -14,16 +15,18 @@ function setup(){
   yVelocity = masterVelocity;
   // We only want to load the logo once.
   dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
+  logoWidth = 200;
+  logoHeight = 150;
 }
 
 function draw(){
   background(220);
   // Draw the logo at the new position.
-  image(dvdImage, x, y, 200, 150);
-  if (x > 600 || x < 0){
+  image(dvdImage, x, y, logoWidth, logoHeight);
+  if (x > width - logoWidth || x < 0){
     xVelocity = -1*xVelocity;
   }
-  if (y > 400 || y < 0){
+  if (y > height - logoHeight || y < 0){
     yVelocity = -1*yVelocity;
   }
   x += xVelocity;
