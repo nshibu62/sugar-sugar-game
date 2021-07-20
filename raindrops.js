0206339e36909721915abe71dddbeed92ecfe012
Glitch (wiry-plaid-valley) <none>
2021-07-20 13:26:12 +0000

    🚖🍼 Checkpoint ./raindrops.js:31745905/1823

--- raindrops.js
+++ raindrops.js
@@ -3,59 +3,41 @@ background, ellipse, text, stroke, line, globalS, globalB
 width, height, mouseX, mouseY, rect, ellipse, random
 mouseIsPressed, priorX, priorY, collideCircleCircle
 keyCode, UP_ARROW, textSize, DOWN_ARROW, RIGHT_ARROW, LEFT_ARROW, consol, collideRectCircle
-drop
 */
 
-let drop1x, drop1y, drop1d, drop1FallSpeed, drops, drop1, drop2, drop3, drop4;
+let drop1x, drop1y, drop1d, drop1FallSpeed;
+
 function setup() {
   createCanvas(500, 500);
-  colorMode(HSB, 360, 100, 100);
+  colorMode(HSB, 100);
   // Variables for droplet 1
-  drop1 = {
-    x: 200,
-    y: 0,
-    d: 14,
-    fallSpeed: 8
-  };
+  drop1x = 200; // or random(width)
+  drop1y = 0; // or random(height)
+  drop1d = 10; // or random(5,15)
+  drop1FallSpeed = 8; // or random(8, 20)
   // Variables for droplet 2
-  drop2 = {
-    x: random(width),
-    y: random(height),
-    d: 8,
-    fallSpeed: 7
-  };
-  drop3 = {
-    x: random(width),
-    y: random(height),
-    d: 9,
-    fallSpeed: 9
-  };
-  drop4 = {
-    x: random(width),
-    y: random(height),
-    d: 6,
-    fallSpeed: 12
-  };
-  drops = [drop1, drop2, drop3, drop4];
 }
+
 function draw() {
-  background(95);
-  noStroke();
-  fill(200,80,50);
-  for (let i = 0; i < 4; i++) {
-    drop = drops[i];
-    drop.y += drop.fallSpeed;
-    if (drop.y > height) {
-      // ...reset it...
-      drop.y = 0;
-      // ...and move it somewhere random.
-      drop.x = random(width);
-    }
-    ellipse(drop.x, drop.y, drop.d);
+  background(0, 0, 95);
+  //// Code for droplet 1
+  // Move droplet 1
+  drop1y += drop1FallSpeed;
+  // If it goes off the screen...
+  if (drop1y > height) {
+    // ...reset it...
+    drop1y = 0;
+    // ...and move it somewhere random.
+    drop1x = random(width);
   }
+  // Display droplet 1
+  noStroke();
+  fill(60, 80, 80);
+  ellipse(drop1x, drop1y, drop1d);
+  //// Code for droplet 2
+  // Code your next droplet here
 }
 
 function mousePressed() {
- const drop = drops[0];
- console.log(drop.x);
+  console.log(drop1x);
 }
