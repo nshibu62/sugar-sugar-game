e3c93f894fa2831c27f0fb78da60e8cd3df5899b
Glitch (wiry-plaid-valley) <none>
2021-08-02 15:50:44 +0000

    🏉🎤 Checkpoint

---
+++ sugar.js
@@ -0,0 +1,24 @@
+/* global createCanvas, colorMode, HSB, color, noStroke, fill, noFill, strokeWeight,
+background, ellipse, text, stroke, line, globalS, globalB
+width, height, mouseX, mouseY, rect, ellipse, random, createSlider
+mouseIsPressed, priorX, priorY, collideCircleCircle,loadImage,image
+keyCode, UP_ARROW, textSize, DOWN_ARROW, RIGHT_ARROW, LEFT_ARROW, consol, collideRectCircle, append
+*/
+//Eban, Divita, Nisha
+
+let backgroundColor;
+
+
+function setup() {
+  createCanvas(600, 500);
+  colorMode(HSB, 360, 100, 100);
+  backgroundColor = (32, 95, 61);
+  
+
+}
+
+function draw() {
+  background(backgroundColor);
+
+
+}
