a9be5aa7890222c874fd6ba658c1ba0cb7d58f3b
Glitch (wiry-plaid-valley) <none>
2021-08-03 12:22:13 +0000

    🎅🚇 Checkpoint ./sugar.js:33133975/444

--- sugar.js
+++ sugar.js
@@ -2,11 +2,11 @@
 background, ellipse, text, stroke, line, globalS, globalB
 width, height, mouseX, mouseY, rect, ellipse, random, createSlider
 mouseIsPressed, priorX, priorY, collideCircleCircle,loadImage,image
-keyCode, UP_ARROW, textSize, DOWN_ARROW, RIGHT_ARROW, LEFT_ARROW, consol, collideRectCircle, append
+keyCode, UP_ARROW, textSize, DOWN_ARROW, RIGHT_ARROW, LEFT_ARROW, consol, collideRectCircle, append, point
 */
 //Eban, Divita, Nisha
 
-let backgroundColor;
+let backgroundColor, line_points;
 
 
 function setup() {
@@ -14,11 +14,28 @@ function setup() {
   colorMode(HSB, 360, 100, 100);
   backgroundColor = (32, 95, 61);
   
+  line_points = [];
 
 }
 
 function draw() {
   background(backgroundColor);
 
+  
 
+}
+
+function mousePressed(){
+   stroke(0);
+   //point(mouseX, mouseY);
+   line_points.push(mouseX, mouseY);
+  line_point.push(mouseY);
+   console.log(line_points);
+   console.log(line_points.length);
+
+    if(line_points.length === 4){
+        line(line_points[0],line_points[1],line_points[2],line_points[3]);
+        line_points = [];
+    }
+  
 }
